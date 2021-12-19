d4fe8678f0a0d3b9568a85041cb43cbd0c2de360
Kamil Khismatullin <kami16ru@gmail.com>
2021-12-19 14:53:17 +0300

    setup method parametrs: props and context

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <h1>{{ name }}</h1>
   <button  @click="placeOrder">Place Order</button>
+
+  <YummyMeal name="Gamburger" :price="5" @addToCard="addItemToCard" />
+
 </template>
 
 <script>
+import YummyMeal from '@/components/YummyMeal.vue'
+
 export default {
+  components: { YummyMeal },
   setup() {
-    const name = "The Snazzy Burger";
+    const name = 'The Snazzy Burger'
+    const placeOrder = () => alert('You\'re order has been placed!')
+    const addItemToCard = (item) => alert(`One ${item} added to card!`)
 
-    const placeOrder = () => alert("You're order has been placed!")
-
-    return { name, placeOrder };
+    return { name, placeOrder, addItemToCard };
   }
 }
 
---
+++ src/components/YummyMeal.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>
+    <strong>{{ name }}</strong>
+    {{ price }}
+    <button @click="addToCard">Add to Card</button>
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    price: Number
+  },
+  setup(props, { emit }) {
+    const addToCard = () => emit('addToCard', props.name)
+
+    return { addToCard }
+  }
+}
+</script>
