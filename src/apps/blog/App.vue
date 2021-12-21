1f573791c2b5ba8e9a7bf8a40e389d02e937a6b0
Kamil Khismatullin <kami16ru@gmail.com>
2021-12-21 23:47:23 +0300

    multiple apps init

---
+++ src/apps/blog/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    I am blog app
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import BasicsApp from './apps/basics/App.vue'
+import BlogApp from './apps/blog/App.vue'
+
+const App = (() => {
+    switch (import.meta.env.VITE_APP_APP) {
+        case 'basics': return BasicsApp
+        case 'blog': return BlogApp
+        default: return BasicsApp
+    }
+})()
 
 createApp(App).mount('#app')
